29d8795b5e5668c26c73fa119e2ca4470c06d4d5
Fabrice Payet <payet.fab@gmail.com>
2018-03-10 18:52:45 +0400

    improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
+  <div
+    style={{
+      margin: '0 auto',
+      maxWidth: 960,
+      padding: '1.45rem 1.0875rem',
+    }}
+  >
     <h1>Il n'y a rien ici...</h1>
+    <img
+      src="https://media.giphy.com/media/9J7tdYltWyXIY/giphy.gif"
+      alt="404 page gif"
+    />
   </div>
 )
 
