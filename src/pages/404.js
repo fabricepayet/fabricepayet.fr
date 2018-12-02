b27ae31a0adf781c69e24eeaa400a514ead93647
Fabrice Payet <payet.fab@gmail.com>
2018-12-02 15:33:37 +0400

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios'
+import Link from 'gatsby-link';
 
-const NotFoundPage = () => (
-  <div
-    style={{
-      margin: '0 auto',
-      maxWidth: 960,
-      padding: '1.45rem 1.0875rem',
-    }}
-  >
-    <h1>Oups ! Il n'y a rien ici...</h1>
-  </div>
-)
+export default class NotFoundPage extends Component {
+  constructor(props) {
+    super(props);
 
-export default NotFoundPage
+    this.state = {
+      imageUrl: ''
+    }
+  }
+
+  componentWillMount() {
+    axios.get('https://api.thecatapi.com/v1/images/search?size=full')
+      .then(response => {
+        this.setState({
+          imageUrl: response.data[0].url
+        })
+      }).catch(error => {
+        console.error(error)
+      })
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '1.45rem 1.0875rem',
+          textAlign: 'center'
+        }}
+      >
+        <Link to="/"><i className="fa fa-arrow-left"></i> Retour</Link>
+        <h1 style={{ textAlign: 'center' }}>Miaouss ! Il n'y a rien ici...</h1>
+        {this.state.imageUrl ?
+          <img src={this.state.imageUrl} alt="cat not found" /> :
+          ''
+        }
+      </div>
+    )
+  }
+}
