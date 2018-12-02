f87e909d565225efa8ad48ed664e864a782fc00e
Fabrice Payet <payet.fab@gmail.com>
2018-12-02 16:02:53 +0400

    update react article

--- src/components/Mailchimp.js
+++ src/components/Mailchimp.js
@@ -68,7 +68,7 @@ export default class MailChimp extends Component {
             <div>
 
               <div>
-                <div style={{ color: "#6d55c1", fontStyle: 'italic', textAlign: 'center' }}>
+                <div style={{ color: "#6d55c1", fontStyle: 'italic', textAlign: 'center', maxWidth: '460px', margin: '12px auto' }}>
                   Vous avez aimé cet article ? Recevez le prochain directement dans votre boite email
                     </div>
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
